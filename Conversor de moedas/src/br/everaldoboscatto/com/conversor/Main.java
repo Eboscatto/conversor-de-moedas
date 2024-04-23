63fbb49be0ddd1ab7d557c83b81e0d6dc9e4ddb5
Eboscaatto <e_boscatto@hotmail.com>
2024-04-23 17:41:49 -0300

    Classes criadas

---
+++ Conversor de moedas/src/br/everaldoboscatto/com/conversor/Main.java
@@ -0,0 +1,7 @@
+package br.everaldoboscatto.com.conversor;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Conversor de moedas/src/br/everaldoboscatto/com/conversor/modelos/Moeda.java
@@ -0,0 +1,4 @@
+package br.everaldoboscatto.com.conversor.modelos;
+
+public class Moeda {
+}
